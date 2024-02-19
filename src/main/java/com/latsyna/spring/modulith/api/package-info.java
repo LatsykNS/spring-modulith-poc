990c56d4c7270b7604e7a12973e6e67415471988
Nazariy Latsyk <Nazariy.Latsyk@aerlingus.com>
2024-02-19 21:29:10 +0200

    Add modulith moments, actuator and some other improvements.

--- src/main/java/com/latsyna/spring/modulith/api/package-info.java
+++ src/main/java/com/latsyna/spring/modulith/api/package-info.java
@@ -1,5 +1,7 @@
 //allow module dependencies
 @org.springframework.modulith.ApplicationModule(
-    allowedDependencies = {"product", "notification"}
+    allowedDependencies = {"product", "notification"
+            //, "product::internal"
+    }
 )
 package com.latsyna.spring.modulith.api;
---
+++ src/test/java/com/latsyna/spring/modulith/api/NotificationApiTest.java
@@ -0,0 +1,40 @@
+package com.latsyna.spring.modulith.api;
+
+import com.latsyna.spring.modulith.notification.Notification;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.modulith.moments.support.TimeMachine;
+import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.modulith.test.PublishedEvents;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.Duration;
+
+
+/**
+ * Test for {@link NotificationApi}.
+ */
+@ApplicationModuleTest(ApplicationModuleTest.BootstrapMode.DIRECT_DEPENDENCIES)
+@TestPropertySource(properties = "spring.modulith.moments.enable-time-machine=true")
+class NotificationApiTest {
+
+
+    @Autowired
+    private NotificationApi notificationApi;
+
+    @Autowired
+    private TimeMachine timeMachine;
+
+    @Test
+    void sendNotification() {
+        notificationApi.sendNotification(new Notification("test"));
+    }
+
+
+    @Test
+    void sendNotificationToCustomer(final PublishedEvents publishedEvents) {
+        for (var i = 0; i < 8; i++) {
+            timeMachine.shiftBy(Duration.ofDays(i));
+        }
+    }
+}
